065636bba5fe8e8fa42d2026ed00261709680bb2
Omr-pseudo <uali2407@gmail.com>
2022-08-13 22:52:28 +0500

    Feat: Accounts button added to let user reselect

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,8 @@
+import { useContext } from 'react';
 import { HiMenuAlt4 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
+
+import { TransactionContext } from '../context/TransactionContext';
 
 import Logo from '../../images/logo.png'
 import React from 'react';
@@ -16,6 +19,8 @@ const Navbar = () => {
 
     const [toggleMenu, setToggleMenu] = React.useState(false);
 
+    const { promptWallet }  = useContext(TransactionContext);
+
     return (
         <nav className='w-full flex md:px-24 justify-between items-center px-4'>
             <div className='md:flex-[0.5] flex-initial justify-center items-center'>
@@ -23,12 +28,20 @@ const Navbar = () => {
              </div>
 
              <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
-                {["Market","Exchange","Tutorials","Wallets"].map((item,index) =>(
+                {["Reliable","Cryptocurrency","Exchange","Guaranteed"].map((item,index) =>(
                     <NavbarItem key={item+index} title={item}/>
                 ))}
 
-                <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
-                    Login
+                <li>
+                <button
+                        type="button"
+                        onClick={promptWallet}
+                        className="flex flex-row justify-center items-center bg-[#2952e3] py-2 px-4 rounded-full cursor-pointer hover:bg-[#2546bd]"
+                        >
+                        <p className="text-white text-base font-semibold">
+                            Accounts
+                        </p>
+                        </button>
                 </li>
              </ul>
             
@@ -41,17 +54,30 @@ const Navbar = () => {
                     }
                     {
                         toggleMenu && (
-                            <ul className=' z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+                            <ul className=' z-10 fixed top-0 -right-2 p-8 w-[70vw] h-screen shadow-2xl md:hidden list-none
                                 flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in'
                             
                             >
-                                <li className='text-xl w-full my-2'>
+                                <li className='text-xl w-full'>
                                     <AiOutlineClose onClick={() => setToggleMenu(false)}/>
                                 </li>
                                 
-                                {["Market","Exchange","Tutorials","Wallets"].map((item,index) =>(
+                                <li>
+                                <button
+                                        type="button"
+                                        onClick={promptWallet}
+                                        className="flex flex-row justify-center items-center bg-[#2952e3] py-2 my-6 px-12 rounded-full cursor-pointer hover:bg-[#2546bd]"
+                                        >
+                                        <p className="text-white text-base font-semibold">
+                                            Accounts
+                                        </p>
+                                        </button>
+                                </li>
+
+                                {["Reliable","Cryptocurrency","Exchange","Guaranteed"].map((item,index) =>(
                                     <NavbarItem key={item+index} title={item} classProps="my-2 text-lg"/>
                                 ))}
+                                
                             </ul>
 
                         )
